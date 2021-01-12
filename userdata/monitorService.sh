c63dfb7a67c8f2a0507c0602d740248a021e24e0
JoeNeu <37147189+JoeNeu@users.noreply.github.com>
2021-01-12 02:11:24 +0100

    fixed grafana configs

--- userdata/monitorService.sh
+++ userdata/monitorService.sh
@@ -42,14 +42,14 @@ scrape_configs:
 echo """
 apiVersion: 1
 datasources:
-- name: Prometheus
-  type: prometheus
-  access: proxy
-  orgId: 1
-  url: http://prometheus:9090
-  version: 1
-  editable: false
-""" >> /etc/grafana/provisioning/datasources/datasource.yml
+  - name: Prometheus
+    type: prometheus
+    access: proxy
+    orgId: 1
+    url: http://prometheus:9090
+    version: 1
+    editable: false
+""" >> /etc/datasource.yaml
 
 # Grafana notifier webhooks
 echo """
@@ -59,8 +59,8 @@ notifiers:
     uid: scale-up
     is_default: false
     send_reminder: true
-    disable_resolve_message: true
     frequency: '5m'
+    disable_resolve_message: true
     settings:
       autoResolve: true
       httpMethod: 'POST'
@@ -72,386 +72,31 @@ notifiers:
     uid: scale-down
     is_default: false
     send_reminder: true
-    disable_resolve_message: true
     frequency: '5m'
+    disable_resolve_message: true
     settings:
       autoResolve: true
       httpMethod: 'POST'
       severity: 'critical'
       uploadImage: false
       url: 'http://autoscaler:8090/down'
-""" >> /etc/grafana/provisioning/notifiers/notifiers.yml
+""" >> /etc/notifiers.yaml
 
 # Create dashboard configuration
 echo """
 apiVersion: 1
 providers:
-- name: 'Home'
-  orgId: 1
-  folder: ''
-  type: file
-  updateIntervalSeconds: 10
-  options:
-    path: /etc/grafana/dashboards
-""" >> /etc/grafana/provisioning/dashboards/dashboards.yml
+  - name: 'Home'
+    orgId: 1
+    folder: ''
+    type: file
+    updateIntervalSeconds: 10
+    options:
+      path: /etc/grafana/dashboards
+""" >> /etc/dashboards.yaml
 
 # Dashboard configuration
-# shellcheck disable=SC2028
-echo """
-{
-  \"annotations\": {
-    \"list\": [
-      {
-        \"builtIn\": 1,
-        \"datasource\": \"-- Grafana --\",
-        \"enable\": true,
-        \"hide\": true,
-        \"iconColor\": \"rgba(0, 211, 255, 1)\",
-        \"name\": \"Annotations & Alerts\",
-        \"type\": \"dashboard\"
-      }
-    ]
-  },
-  \"editable\": true,
-  \"gnetId\": null,
-  \"graphTooltip\": 0,
-  \"id\": 1,
-  \"links\": [],
-  \"panels\": [
-    {
-      \"alert\": {
-        \"alertRuleTags\": {},
-        \"conditions\": [
-          {
-            \"evaluator\": {
-              \"params\": [
-                0.8
-              ],
-              \"type\": \"gt\"
-            },
-            \"operator\": {
-              \"type\": \"and\"
-            },
-            \"query\": {
-              \"params\": [
-                \"A\",
-                \"1m\",
-                \"now\"
-              ]
-            },
-            \"reducer\": {
-              \"params\": [],
-              \"type\": \"avg\"
-            },
-            \"type\": \"query\"
-          }
-        ],
-        \"executionErrorState\": \"alerting\",
-        \"for\": \"1m\",
-        \"frequency\": \"1m\",
-        \"handler\": 1,
-        \"name\": \"High CPU alert\",
-        \"noDataState\": \"no_data\",
-        \"notifications\": [
-          {
-            \"uid\": \"scale-up\"
-          }
-        ]
-      },
-      \"aliasColors\": {},
-      \"bars\": false,
-      \"dashLength\": 10,
-      \"dashes\": false,
-      \"datasource\": \"Prometheus\",
-      \"fieldConfig\": {
-        \"defaults\": {
-          \"custom\": {},
-          \"mappings\": [],
-          \"thresholds\": {
-            \"mode\": \"absolute\",
-            \"steps\": [
-              {
-                \"color\": \"green\",
-                \"value\": null
-              },
-              {
-                \"color\": \"red\",
-                \"value\": 80
-              }
-            ]
-          },
-          \"unit\": \"percentunit\"
-        },
-        \"overrides\": []
-      },
-      \"fill\": 1,
-      \"fillGradient\": 0,
-      \"gridPos\": {
-        \"h\": 9,
-        \"w\": 12,
-        \"x\": 0,
-        \"y\": 0
-      },
-      \"hiddenSeries\": false,
-      \"id\": 2,
-      \"legend\": {
-        \"avg\": false,
-        \"current\": false,
-        \"max\": false,
-        \"min\": false,
-        \"show\": true,
-        \"total\": false,
-        \"values\": false
-      },
-      \"lines\": true,
-      \"linewidth\": 1,
-      \"nullPointMode\": \"null\",
-      \"options\": {
-        \"alertThreshold\": true
-      },
-      \"percentage\": false,
-      \"pluginVersion\": \"7.3.6\",
-      \"pointradius\": 2,
-      \"points\": false,
-      \"renderer\": \"flot\",
-      \"seriesOverrides\": [],
-      \"spaceLength\": 10,
-      \"stack\": false,
-      \"steppedLine\": false,
-      \"targets\": [
-        {
-          \"expr\": \"avg(\r\n    sum by (instance) (rate(node_cpu_seconds_total{mode!=\"idle\"}[1m])) /\r\n    sum by (instance) (rate(node_cpu_seconds_total[1m]))\r\n)\",
-          \"instant\": false,
-          \"interval\": \"\",
-          \"legendFormat\": \"\",
-          \"refId\": \"A\"
-        }
-      ],
-      \"thresholds\": [
-        {
-          \"colorMode\": \"critical\",
-          \"fill\": true,
-          \"line\": true,
-          \"op\": \"gt\",
-          \"value\": 0.8
-        }
-      ],
-      \"timeFrom\": null,
-      \"timeRegions\": [],
-      \"timeShift\": null,
-      \"title\": \"High CPU\",
-      \"tooltip\": {
-        \"shared\": true,
-        \"sort\": 0,
-        \"value_type\": \"individual\"
-      },
-      \"type\": \"graph\",
-      \"xaxis\": {
-        \"buckets\": null,
-        \"mode\": \"time\",
-        \"name\": null,
-        \"show\": true,
-        \"values\": []
-      },
-      \"yaxes\": [
-        {
-          \"format\": \"percentunit\",
-          \"label\": null,
-          \"logBase\": 1,
-          \"max\": \"1\",
-          \"min\": \"0\",
-          \"show\": true
-        },
-        {
-          \"format\": \"short\",
-          \"label\": null,
-          \"logBase\": 1,
-          \"max\": \"1\",
-          \"min\": \"0\",
-          \"show\": true
-        }
-      ],
-      \"yaxis\": {
-        \"align\": false,
-        \"alignLevel\": null
-      }
-    },
-    {
-      \"alert\": {
-        \"alertRuleTags\": {},
-        \"conditions\": [
-          {
-            \"evaluator\": {
-              \"params\": [
-                0.2
-              ],
-              \"type\": \"lt\"
-            },
-            \"operator\": {
-              \"type\": \"and\"
-            },
-            \"query\": {
-              \"params\": [
-                \"A\",
-                \"1m\",
-                \"now\"
-              ]
-            },
-            \"reducer\": {
-              \"params\": [],
-              \"type\": \"avg\"
-            },
-            \"type\": \"query\"
-          }
-        ],
-        \"executionErrorState\": \"alerting\",
-        \"for\": \"1m\",
-        \"frequency\": \"1m\",
-        \"handler\": 1,
-        \"name\": \"Low CPU alert\",
-        \"noDataState\": \"no_data\",
-        \"notifications\": [
-          {
-            \"uid\": \"scale-down\"
-          }
-        ]
-      },
-      \"aliasColors\": {},
-      \"bars\": false,
-      \"dashLength\": 10,
-      \"dashes\": false,
-      \"datasource\": \"Prometheus\",
-      \"fieldConfig\": {
-        \"defaults\": {
-          \"custom\": {},
-          \"mappings\": [],
-          \"thresholds\": {
-            \"mode\": \"absolute\",
-            \"steps\": [
-              {
-                \"color\": \"green\",
-                \"value\": null
-              },
-              {
-                \"color\": \"red\",
-                \"value\": 80
-              }
-            ]
-          },
-          \"unit\": \"percentunit\"
-        },
-        \"overrides\": []
-      },
-      \"fill\": 1,
-      \"fillGradient\": 0,
-      \"gridPos\": {
-        \"h\": 9,
-        \"w\": 12,
-        \"x\": 12,
-        \"y\": 0
-      },
-      \"hiddenSeries\": false,
-      \"id\": 3,
-      \"legend\": {
-        \"avg\": false,
-        \"current\": false,
-        \"max\": false,
-        \"min\": false,
-        \"show\": true,
-        \"total\": false,
-        \"values\": false
-      },
-      \"lines\": true,
-      \"linewidth\": 1,
-      \"nullPointMode\": \"null\",
-      \"options\": {
-        \"alertThreshold\": true
-      },
-      \"percentage\": false,
-      \"pluginVersion\": \"7.3.6\",
-      \"pointradius\": 2,
-      \"points\": false,
-      \"renderer\": \"flot\",
-      \"seriesOverrides\": [],
-      \"spaceLength\": 10,
-      \"stack\": false,
-      \"steppedLine\": false,
-      \"targets\": [
-        {
-          \"expr\": \"avg(\r\n    sum by (instance) (rate(node_cpu_seconds_total{mode!=\"idle\"}[1m])) /\r\n    sum by (instance) (rate(node_cpu_seconds_total[1m]))\r\n)\",
-          \"instant\": false,
-          \"interval\": \"\",
-          \"legendFormat\": \"\",
-          \"refId\": \"A\"
-        }
-      ],
-      \"thresholds\": [
-        {
-          \"colorMode\": \"critical\",
-          \"fill\": true,
-          \"line\": true,
-          \"op\": \"lt\",
-          \"value\": 0.2
-        }
-      ],
-      \"timeFrom\": null,
-      \"timeRegions\": [],
-      \"timeShift\": null,
-      \"title\": \"Low CPU\",
-      \"tooltip\": {
-        \"shared\": true,
-        \"sort\": 0,
-        \"value_type\": \"individual\"
-      },
-      \"type\": \"graph\",
-      \"xaxis\": {
-        \"buckets\": null,
-        \"mode\": \"time\",
-        \"name\": null,
-        \"show\": true,
-        \"values\": []
-      },
-      \"yaxes\": [
-        {
-          \"format\": \"percentunit\",
-          \"label\": null,
-          \"logBase\": 1,
-          \"max\": \"1\",
-          \"min\": \"0\",
-          \"show\": true
-        },
-        {
-          \"format\": \"short\",
-          \"label\": null,
-          \"logBase\": 1,
-          \"max\": \"1\",
-          \"min\": \"0\",
-          \"show\": true
-        }
-      ],
-      \"yaxis\": {
-        \"align\": false,
-        \"alignLevel\": null
-      }
-    }
-  ],
-  \"schemaVersion\": 26,
-  \"style\": \"dark\",
-  \"tags\": [],
-  \"templating\": {
-    \"list\": []
-  },
-  \"time\": {
-    \"from\": \"now-6h\",
-    \"to\": \"now\"
-  },
-  \"timepicker\": {},
-  \"timezone\": \"\",
-  \"title\": \"autoscaler\",
-  \"uid\": \"Zt9HEuaGk\",
-  \"version\": 10
-}
-""" >> /etc/grafana/dashboards/dashboard.json
+printf '{"annotations":{"list":[{"builtIn":1,"datasource":"-- Grafana --","enable":true,"hide":true,"iconColor":"rgba(0, 211, 255, 1)","name":"Annotations & Alerts","type":"dashboard"}]},"editable":true,"gnetId":null,"graphTooltip":0,"id":1,"links":[],"panels":[{"alert":{"alertRuleTags":{},"conditions":[{"evaluator":{"params":[0.8],"type":"gt"},"operator":{"type":"and"},"query":{"params":["A","1m","now"]},"reducer":{"params":[],"type":"avg"},"type":"query"}],"executionErrorState":"alerting","for":"1m","frequency":"1m","handler":1,"name":"High CPU alert","noDataState":"no_data","notifications":[{"uid":"scale-up"}]},"aliasColors":{},"bars":false,"dashLength":10,"dashes":false,"datasource":"Prometheus","fieldConfig":{"defaults":{"custom":{},"mappings":[],"thresholds":{"mode":"absolute","steps":[{"color":"green","value":null},{"color":"red","value":80}]},"unit":"percentunit"},"overrides":[]},"fill":1,"fillGradient":0,"gridPos":{"h":9,"w":12,"x":0,"y":0},"hiddenSeries":false,"id":2,"legend":{"avg":false,"current":false,"max":false,"min":false,"show":true,"total":false,"values":false},"lines":true,"linewidth":1,"nullPointMode":"null","options":{"alertThreshold":true},"percentage":false,"pluginVersion":"7.3.6","pointradius":2,"points":false,"renderer":"flot","seriesOverrides":[],"spaceLength":10,"stack":false,"steppedLine":false,"targets":[{"expr":"avg(    sum by (instance) (rate(node_cpu_seconds_total{mode!=\"idle\"}[1m])) \/    sum by (instance) (rate(node_cpu_seconds_total[1m])))","instant":false,"interval":"","legendFormat":"","refId":"A"}],"thresholds":[{"colorMode":"critical","fill":true,"line":true,"op":"gt","value":0.8}],"timeFrom":null,"timeRegions":[],"timeShift":null,"title":"High CPU","tooltip":{"shared":true,"sort":0,"value_type":"individual"},"type":"graph","xaxis":{"buckets":null,"mode":"time","name":null,"show":true,"values":[]},"yaxes":[{"format":"percentunit","label":null,"logBase":1,"max":"1","min":"0","show":true},{"format":"short","label":null,"logBase":1,"max":"1","min":"0","show":true}],"yaxis":{"align":false,"alignLevel":null}},{"alert":{"alertRuleTags":{},"conditions":[{"evaluator":{"params":[0.2],"type":"lt"},"operator":{"type":"and"},"query":{"params":["A","1m","now"]},"reducer":{"params":[],"type":"avg"},"type":"query"}],"executionErrorState":"alerting","for":"1m","frequency":"1m","handler":1,"name":"Low CPU alert","noDataState":"no_data","notifications":[{"uid":"scale-down"}]},"aliasColors":{},"bars":false,"dashLength":10,"dashes":false,"datasource":"Prometheus","fieldConfig":{"defaults":{"custom":{},"mappings":[],"thresholds":{"mode":"absolute","steps":[{"color":"green","value":null},{"color":"red","value":80}]},"unit":"percentunit"},"overrides":[]},"fill":1,"fillGradient":0,"gridPos":{"h":9,"w":12,"x":12,"y":0},"hiddenSeries":false,"id":3,"legend":{"avg":false,"current":false,"max":false,"min":false,"show":true,"total":false,"values":false},"lines":true,"linewidth":1,"nullPointMode":"null","options":{"alertThreshold":true},"percentage":false,"pluginVersion":"7.3.6","pointradius":2,"points":false,"renderer":"flot","seriesOverrides":[],"spaceLength":10,"stack":false,"steppedLine":false,"targets":[{"expr":"avg( sum by (instance) (rate(node_cpu_seconds_total{mode!=\"idle\"}[1m])) \/   sum by (instance) (rate(node_cpu_seconds_total[1m])))","instant":false,"interval":"","legendFormat":"","refId":"A"}],"thresholds":[{"colorMode":"critical","fill":true,"line":true,"op":"lt","value":0.2}],"timeFrom":null,"timeRegions":[],"timeShift":null,"title":"Low CPU","tooltip":{"shared":true,"sort":0,"value_type":"individual"},"type":"graph","xaxis":{"buckets":null,"mode":"time","name":null,"show":true,"values":[]},"yaxes":[{"format":"percentunit","label":null,"logBase":1,"max":"1","min":"0","show":true},{"format":"short","label":null,"logBase":1,"max":"1","min":"0","show":true}],"yaxis":{"align":false,"alignLevel":null}}],"schemaVersion":26,"style":"dark","tags":[],"templating":{"list":[]},"time":{"from":"now-6h","to":"now"},"timepicker":{},"timezone":"","title":"autoscaler","uid":"Zt9HEuaGk","version":10}' >> /etc/dashboard.json
 
 # Create docker network
 docker network create monitoring
@@ -498,8 +143,8 @@ docker run -d \
   --name=grafana \
   --restart=always \
   --net=monitoring \
-  -v /etc/grafana/provisioning/datasources/datasource.yml:/etc/grafana/provisioning/datasources/datasource.yml \
-  -v /etc/grafana/provisioning/notifiers/notifiers.yml:/etc/grafana/provisioning/notifiers/notifiers.yml \
-  -v /etc/grafana/provisioning/dashboards/dashboards.yml:/etc/grafana/provisioning/dashboards/dashboards.yml \
-  -v /etc/grafana/dashboards/dashboard.json:/etc/grafana/dashboards/dashboard.json \
+  -v /etc/datasource.yaml:/etc/grafana/provisioning/datasources/datasource.yaml \
+  -v /etc/notifiers.yaml:/etc/grafana/provisioning/notifiers/notifiers.yaml \
+  -v /etc/dashboards.yaml:/etc/grafana/provisioning/dashboards/dashboards.yaml \
+  -v /etc/dashboard.json:/etc/grafana/dashboards/dashboard.json \
   grafana/grafana
